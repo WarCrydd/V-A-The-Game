a8f95667aec3d1e4725c6847b0d9327a43bddbcf
Ákos Varga <IS850J@student.uni-pannon.hu>
2021-03-24 14:17:40 +0100

    frisités

--- V-A-The-Game/DefaultAdatok.h
+++ V-A-The-Game/DefaultAdatok.h
@@ -14,7 +14,7 @@ namespace Kijelzo
     static unsigned int magassag = 900;
     static unsigned int max_nagyitas = 1000;
     static unsigned int min_nagyitas = 1000;
-    static bool fullScreen = true;
+    static bool fullScreen = false;
     static string felirat = "A valami";
     static unsigned int alapMaxFPS = 100;
 }
